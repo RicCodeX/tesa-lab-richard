e0ff4e7666d95af013fec58f9fc6e20f07c96378
timirichy <tianchris198@gmail.com>
2025-09-21 03:13:36 +0100

    build 2 confirmation

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def hello():
-    return "CI/CD ✅ Hello from Richard via GitHub Actions again → EC2 (port :8000)"
+    return "CI/CD ✅ Deployed by GitHub Actions (build #2)"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
